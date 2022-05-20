1261b61b350460edac54c10111a8edb163dff78f
Ivan Ocampo <loqueyoquieroesunugaton98@hotmail.com>
2022-05-19 23:26:26 -0300

    doceavo commit

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import  ItemList  from "../ItemList/ItemList";
-
+import { collection, getDocs, getFirestore} from 'firebase/firestore';
 function traerProductos(category) {
   const myPromise = new Promise((resolve, reject) => {
         console.log();
     const productList = [
-      { id: "1", img:"https://images.cdn1.buscalibre.com/fit-in/360x360/66/1a/661a3760157941a94cb8db3f5a9d5060.jpg", category: "libros", title: "El señor de los anillos", price: "$300" },
+      { id: "1", img:"", category: "libros", title: "El señor de los anillos", price: "$300" },
       { id: "2", img:"", category: "libros", title: "Harry Potter", price: "$400" },
       { id: "3", img:"", category: "comics", title: "Crepusculo", price: "$500" },
       { id: "4", img:"", category: "revistas", title: "El resplandor ", price: "$350" },
@@ -20,14 +20,26 @@ function traerProductos(category) {
 }
 
 function ItemListContainer() {
-  const [productos, setProductos] = useState([]);
-  const { categoryId } = useParams();
+  const db = getFirestore();
 
-  useEffect(() => {
-    traerProductos(categoryId).then((res) => {
-      setProductos(res);
-    });
-  }, [categoryId]);
+  const itemCollection = collection(db, 'items');
+
+  getDocs(itemCollection)
+  .then(snapshot =>{
+    console.log(snapshot.docs.map(doc => { return {...doc.data(), id: doc.id }
+   }));
+  })
+  .catch(
+    err => console.log(err)
+  );
+  // const [productos, setProductos] = useState([]);
+  // const { categoryId } = useParams();
+
+  // useEffect(() => {
+  //   traerProductos(categoryId).then((res) => {
+  //     setProductos(res);
+  //   });
+  // }, [categoryId]);
 
   return (
     <>
